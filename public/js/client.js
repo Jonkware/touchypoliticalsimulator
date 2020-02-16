234619a26b629f85a09d8cad7ad9984446b98719
Clement-Vivier <52208691+Clement-Vivier@users.noreply.github.com>
2020-02-16 21:20:39 +0100

    test du serveur nodejs
    ajout nodejs client server

---
+++ public/js/client.js
@@ -0,0 +1,35 @@
+const standardBtn = document.getElementById('standardBtn');
+standardBtn.addEventListener('click', function(e) {
+  $(document).ready(function () {
+            $("#choice1").text();
+            $("#choice2").text("toto");
+            $("#choice3").text("toto");
+            $("#yourPlay").text("toto");
+            $("#theOppopentPlay").text("toto");
+        });
+});
+
+const dicatorsBtn = document.getElementById('dicatorsBtn');
+dicatorsBtn.addEventListener('click', function(e) {
+  console.log('button was clicked');
+});
+
+const chinaBtn = document.getElementById('chinaBtn');
+chinaBtn.addEventListener('click', function(e) {
+  console.log('button was clicked');
+});
+
+const gayBtn = document.getElementById('gayBtn');
+gayBtn.addEventListener('click', function(e) {
+  console.log('button was clicked');
+});
+
+const frenchBtn = document.getElementById('frenchBtn');
+frenchBtn.addEventListener('click', function(e) {
+  console.log('button was clicked');
+});
+
+const middleBtn = document.getElementById('middleBtn');
+middleBtn.addEventListener('click', function(e) {
+  console.log('button was clicked');
+});
