3c4c2447896ccf137c0c369f022cb8c8d2e77135
Clement-Vivier <52208691+Clement-Vivier@users.noreply.github.com>
2020-02-17 19:36:28 +0100

    ajout timer, ajout score, gestion timer

--- public/js/client.js
+++ public/js/client.js
@@ -7,7 +7,7 @@ var china;
 var moyenorient;
 var music;
 var lafrance;
-
+var type = 0;
 $(document).ready(function() {
   $("#choice1_input").click( function(e) {
     console.log("First");
@@ -35,6 +35,7 @@ $(document).ready(function() {
 
 standardBtn.addEventListener('click', function(e) {
   $(document).ready(function () {
+  	type = 0;
     $("#choice1").attr("src", standard.pierre.url);
     $("#choice1_text").text(standard.pierre.name);
     $("#choice2").attr("src", standard.feuille.url);
@@ -43,12 +44,15 @@ standardBtn.addEventListener('click', function(e) {
     $("#choice3_text").text(standard.ciseaux.name);
     $("#yourPlay").text("Pas encore voté");
     $("#theOppopentPlay").text("Pas encore voté");
+    uncheckButtonRadio();
+    console.log("toto");
   });
 });
 
 const dicatorsBtn = document.getElementById('dicatorsBtn');
 dicatorsBtn.addEventListener('click', function(e) {
   $(document).ready(function () {
+  	type = 1;
     $("#choice1").attr("src", advanced.kim_jong_un.url);
     $("#choice2").attr("src", advanced.donald_trump.url);
     $("#choice3").attr("src", advanced.poutine.url);
@@ -57,12 +61,14 @@ dicatorsBtn.addEventListener('click', function(e) {
     $("#choice3_text").text(standard.ciseaux.name);
     $("#yourPlay").text("Pas encore voté");
     $("#theOppopentPlay").text("Pas encore voté");
+    uncheckButtonRadio();
   });
 });
 
 const chinaBtn = document.getElementById('chinaBtn');
 chinaBtn.addEventListener('click', function(e) {
   $(document).ready(function () {
+  	type = 2;
     $("#choice1").attr("src", china.coronavirus.url);
     $("#choice2").attr("src", china.liberte.url);
     $("#choice3").attr("src", china.mondialisation.url);
@@ -71,12 +77,14 @@ chinaBtn.addEventListener('click', function(e) {
     $("#choice3_text").text(china.mondialisation.name);
     $("#yourPlay").text("Pas encore voté");
     $("#theOppopentPlay").text("Pas encore voté");
+    uncheckButtonRadio();
   });
 });
 
 const gayBtn = document.getElementById('gayBtn');
 gayBtn.addEventListener('click', function(e) {
   $(document).ready(function () {
+  	type = 3;
     $("#choice1").attr("src", lafrance.lgbt.url);
     $("#choice2").attr("src", lafrance.manif.url);
     $("#choice3").attr("src", lafrance.loi49_3.url);
@@ -85,12 +93,14 @@ gayBtn.addEventListener('click', function(e) {
     $("#choice3_text").text(lafrance.loi49_3.name);
     $("#yourPlay").text("Pas encore voté");
     $("#theOppopentPlay").text("Pas encore voté");
+    uncheckButtonRadio();
   });
 });
 
 const frenchBtn = document.getElementById('frenchBtn');
 frenchBtn.addEventListener('click', function(e) {
   $(document).ready(function () {
+  	type = 4;
     $("#choice1").attr("src", expert.chirac.url);
     $("#choice2").attr("src", expert.macron.url);
     $("#choice3").attr("src", expert.hollande.url);
@@ -99,12 +109,14 @@ frenchBtn.addEventListener('click', function(e) {
     $("#choice3_text").text(expert.hollande.name);
     $("#yourPlay").text("Pas encore voté");
     $("#theOppopentPlay").text("Pas encore voté");
+    uncheckButtonRadio();
   });
 });
 
 const middleBtn = document.getElementById('middleBtn');
 middleBtn.addEventListener('click', function(e) {
   $(document).ready(function () {
+  	type = 5;
     $("#choice1").attr("src", moyenorient.israel.url);
     $("#choice2").attr("src", moyenorient.palestine.url);
     $("#choice3").attr("src", moyenorient.irak.url);
@@ -113,12 +125,14 @@ middleBtn.addEventListener('click', function(e) {
     $("#choice3_text").text(moyenorient.irak.name);
     $("#yourPlay").text("Pas encore voté");
     $("#theOppopentPlay").text("Pas encore voté");
+    uncheckButtonRadio();
   });
 });
 
 const musicBtn = document.getElementById('musicBtn');
 musicBtn.addEventListener('click', function(e) {
   $(document).ready(function () {
+  	type = 6;
     $("#choice1").attr("src", music.trance.url);
     $("#choice2").attr("src", music.hardtek.url);
     $("#choice3").attr("src", music.frenchcore.url);
@@ -127,6 +141,52 @@ musicBtn.addEventListener('click', function(e) {
     $("#choice3_text").text(music.frenchcore.name);
     $("#yourPlay").text("Pas encore voté");
     $("#theOppopentPlay").text("Pas encore voté");
+    uncheckButtonRadio();
   });
+});
+
+function uncheckButtonRadio() {
+	$(document).ready(function () {
+		$("#choice3_input").prop("checked", false);
+		$("#choice2_input").prop("checked", false);
+		$("#choice1_input").prop("checked", false);
+	});
+}
+
+const confirmBtn = document.getElementById('confirmBtn');
+confirmBtn.addEventListener('click', function(e) {
+	uncheckButtonRadio();
+
+	setTimeout(changeImage3,0);
+	setTimeout(changeImage2,1000);
+	setTimeout(changeImage1,2000);
+	setTimeout(resetImage,3000);
+
+	function changeImage3() {
+	  	$(document).ready(function () {
+	  		$("#timerImg").attr("src", "./images/3.png");
+  		});
+	}
+	function changeImage2() {
+	  	$(document).ready(function () {
+	  		$("#timerImg").attr("src", "./images/2.png");
+  		});
+	}
+	function changeImage1() {
+	  	$(document).ready(function () {
+	  		$("#timerImg").attr("src", "./images/1.png");
+  		});
+	}
+	function resetImage() {
+	  	$(document).ready(function () {
+	  		var rand = Math.floor((Math.random() * 3) + 1);
+	  		console.log(type);
+
+	  		if (rand == 3) {
+
+	  		}
+	  		$("#timerImg").attr("src","");
+  		});
+	}
 });
 
